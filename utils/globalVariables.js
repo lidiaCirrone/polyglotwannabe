d8932ad96ef024491d44a65372283f90523be108
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-10 18:47:37 +0200

    added languages array

---
+++ utils/globalVariables.js
@@ -0,0 +1,37 @@
+export const languages = [
+   {
+      label: "Italiano",
+      name: "Italian",
+      slug: "italian",
+   },
+   {
+      label: "English",
+      name: "English",
+      slug: "english",
+   },
+   {
+      label: "Français",
+      name: "French",
+      slug: "french",
+   },
+   {
+      label: "Español",
+      name: "Spanish",
+      slug: "spanish",
+   },
+   {
+      label: "Русский",
+      name: "Russian",
+      slug: "russian",
+   },
+   {
+      label: "Português",
+      name: "Portuguese",
+      slug: "portuguese",
+   },
+   {
+      label: "Svenska",
+      name: "Swedish",
+      slug: "swedish",
+   },
+]
