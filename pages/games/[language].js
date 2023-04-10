f75a54747904622609d00bdf739b779c69a7902f
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-10 18:55:50 +0200

    language data from array

--- pages/games/[language].js
+++ pages/games/[language].js
@@ -1,4 +1,5 @@
 //components
+import Layout from '@/components/layout'
 import Link from 'next/link'
 
 // modules
@@ -7,27 +8,35 @@ import { useRouter } from 'next/router'
 // styles
 import styles from './language.module.css'
 
+//utils
+import { languages } from '@/utils/globalVariables'
+import clsx from 'clsx'
+
 const Language = () => {
-   const router = useRouter()
-   const { language } = router.query
+   const router = useRouter();
+   const { language } = router.query;
+   const languageItem = languages.find(item => item.slug === language);
 
    function goBack() {
       router.back();
    }
 
    return (
-      <section className={styles.container}>
-         <p className={styles.navigation}>
-            <span className={styles.back} onClick={goBack}>
-               ← go back
-            </span>
-            &nbsp;|&nbsp;
-            <Link href="/" className={styles.home} >
-               Home
-            </Link>
-         </p>
-         <p>language param inside router.query: {language}</p>
-      </section>
+      <Layout pageName={languageItem.label} pageDescription={languageItem.label}>
+         <section className={styles.container}>
+            <p className={clsx([styles.navigation, "margin-bottom"])}>
+               <span className={styles.back} onClick={goBack}>
+                  ← go back
+               </span>
+               &nbsp;|&nbsp;
+               <Link href="/" className={styles.home} >
+                  Home
+               </Link>
+            </p>
+            <p className={"margin-bottom"}>language param inside router.query: {language}</p>
+            <p>language item inside array: {JSON.stringify(languageItem)}</p>
+         </section>
+      </Layout>
    )
 }
 
