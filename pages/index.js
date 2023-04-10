db5c58fe2b75c093687bbb71ccfd827f8c7ec9a3
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-10 17:43:42 +0200

    basic grid home layout

--- pages/index.js
+++ pages/index.js
@@ -1,81 +1,24 @@
-// assets
-import { Inter } from 'next/font/google'
-
-// components
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
-
 // styles
 import styles from '@/styles/Home.module.css'
-import utilStyles from '../styles/utils.module.css';
 
 // utils
-import { getSortedPostsData } from '@/lib/posts'
 import Layout from '@/components/layout'
+import clsx from 'clsx'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export async function getStaticProps() {
-   const allPostsData = getSortedPostsData();
-   return {
-      props: {
-         allPostsData
-      }
-   }
-}
-
-export default function Home({ allPostsData }) {
+export default function Home() {
    return (
-      <Layout>
-         <Head>
-            <title>polyglot wannabe</title>
-            <meta name="description" content="unsolicited facts about languages" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/favicon.ico" />
-         </Head>
-
+      <Layout pageName={"polyglot wanna be"} pageDescription={"unsolicited facts about languages"}>
          <main>
-
-            <section className={utilStyles.headingMd}>
-               <p className={inter.className}>[Your Self Introduction]</p>
-               <p className={inter.className}>
-                  (This is a sample website - you’ll be building a site like this on{' '}
-                  <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-               </p>
+            <section className={styles["tiles-container"]}>
+               <div className={clsx([styles.tile, styles["tile-one"]])}>Italiano</div>
+               <div className={clsx([styles.tile, styles["tile-two"]])}>English</div>
+               <div className={clsx([styles.tile, styles["tile-three"]])}>French</div>
+               <div className={clsx([styles.tile, styles["tile-four"]])}>Español</div>
+               <div className={clsx([styles.tile, styles["tile-five"]])}>Русский</div>
+               <div className={clsx([styles.tile, styles["tile-six"]])}>Português</div>
+               <div className={clsx([styles.tile, styles["tile-seven"]])}>Svenska</div>
+               <div className={clsx([styles.tile, styles["tile-eight"]])}>who's next?</div>
             </section>
-
-            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-               <h2 className={inter.className}>Blog</h2>
-               <ul className={utilStyles.list}>
-                  {allPostsData.map(({ id, date, title }) => (
-                     <li className={inter.className} key={id}>
-                        {title}
-                        <br />
-                        {date}
-                     </li>
-                  ))}
-               </ul>
-            </section>
-
-            <div className={styles.grid}>
-               <Link href="games/italiano" className={styles.card}>
-                  <h2 className={inter.className}>
-                     Italiano <span>-&gt;</span>
-                  </h2>
-                  <p className={inter.className}>
-                     aka my native tongue
-                  </p>
-               </Link>
-               <Link href="games/english" className={styles.card}>
-                  <h2 className={inter.className}>
-                     English <span>-&gt;</span>
-                  </h2>
-                  <p className={inter.className}>
-                     the language who <strong>stole</strong> my heart
-                  </p>
-               </Link>
-            </div>
          </main>
       </Layout>
    )
