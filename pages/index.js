bf882960b3e3836731f673ce02605641f25a1361
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-10 18:47:52 +0200

    maps languages instead of hardcoded values

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,32 @@
+// components
+import Layout from '@/components/layout'
+import Link from 'next/link';
+
+// modules
+import clsx from 'clsx'
+
 // styles
 import styles from '@/styles/Home.module.css'
 
 // utils
-import Layout from '@/components/layout'
-import clsx from 'clsx'
+import { languages } from '@/utils/globalVariables'
 
 export default function Home() {
+
+   function renderLanguageTile(language, index) {
+      return (
+         <Link href={`games/${language.slug}`} key={`language-${index}`}>
+            <div className={clsx([styles.tile, styles[`tile${index + 1}`]])}>{language.label}</div>
+         </Link>
+      )
+   }
+
    return (
       <Layout pageName={"polyglot wanna be"} pageDescription={"unsolicited facts about languages"}>
          <main>
             <section className={styles["tiles-container"]}>
-               <div className={clsx([styles.tile, styles["tile-one"]])}>Italiano</div>
-               <div className={clsx([styles.tile, styles["tile-two"]])}>English</div>
-               <div className={clsx([styles.tile, styles["tile-three"]])}>French</div>
-               <div className={clsx([styles.tile, styles["tile-four"]])}>Español</div>
-               <div className={clsx([styles.tile, styles["tile-five"]])}>Русский</div>
-               <div className={clsx([styles.tile, styles["tile-six"]])}>Português</div>
-               <div className={clsx([styles.tile, styles["tile-seven"]])}>Svenska</div>
-               <div className={clsx([styles.tile, styles["tile-eight"]])}>who's next?</div>
+               {languages.map(renderLanguageTile)}
+               <div className={clsx([styles.tile, styles["tile-empty"]])}>who's next?</div>
             </section>
          </main>
       </Layout>
