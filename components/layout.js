096270435abd15e2d813c2de49ec2bdd4e8b8c66
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-10 17:42:49 +0200

    handles dynamic title and meta description tags

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,16 @@
+import Head from "next/head";
 import styles from "./layout.module.css"
 
-export default function Layout({ children }) {
-   return <div className={styles.container}>{children}</div>
+export default function Layout({ pageName, pageDescription, children }) {
+   return (
+      <>
+         <Head>
+            <title>{pageName}</title>
+            <meta name="description" content={pageDescription} />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+         </Head>
+         <div className={styles.container}>{children}</div>
+      </>
+   )
 }
