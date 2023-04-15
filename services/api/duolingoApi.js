957d3270fcd39d62ad980fe1e3d873a2647ddb68
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-15 23:18:36 +0200

    api file

---
+++ services/api/duolingoApi.js
@@ -0,0 +1,3 @@
+export const duolingoResources = {
+   userData: "https://api.duolingo.com/2017-06-30/users/94105502?fields=courses,creationDate,learningLanguage,picture,totalXp,username,streak,trackingProperties"
+}
