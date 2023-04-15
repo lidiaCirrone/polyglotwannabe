ca84c78e4feb4b7af32cbce6ce30e750a48b357d
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-16 00:59:35 +0200

    NavigationBar component

--- pages/games/[language].js
+++ pages/games/[language].js
@@ -1,6 +1,6 @@
 //components
 import Layout from '@/components/layout'
-import Link from 'next/link'
+import NavigationBar from '@/components/ui/navigationBar'
 
 // modules
 import { useRouter } from 'next/router'
@@ -10,29 +10,16 @@ import styles from './language.module.css'
 
 //utils
 import { languages } from '@/utils/globalVariables'
-import clsx from 'clsx'
 
 const Language = () => {
    const router = useRouter();
    const { language } = router.query;
    const languageItem = languages.find(item => item.slug === language);
 
-   function goBack() {
-      router.back();
-   }
-
    return (
       <Layout pageName={languageItem.label} pageDescription={languageItem.label}>
          <section className={styles.container}>
-            <p className={clsx([styles.navigation, "margin-bottom"])}>
-               <span className={styles.back} onClick={goBack}>
-                  ← go back
-               </span>
-               &nbsp;|&nbsp;
-               <Link href="/" className={styles.home} >
-                  Home
-               </Link>
-            </p>
+            <NavigationBar />
             <p className={"margin-bottom"}>language param inside router.query: {language}</p>
             <p>language item inside array: {JSON.stringify(languageItem)}</p>
          </section>
