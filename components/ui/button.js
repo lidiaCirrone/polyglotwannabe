b167c6dcebc2789970231ce3c2f39872f6e3689a
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-15 23:36:17 +0200

    missing import

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,3 +1,6 @@
+// components
+import Link from 'next/link';
+
 export default function Button({ href, label }) {
    return (
       <div className={"button"}><Link href={href}>{label}</Link></div>
