e86591b2308c6b6aea9f32abfd21c4077c4683dc
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-16 00:58:26 +0200

    added `code` to languages array

--- utils/globalVariables.js
+++ utils/globalVariables.js
@@ -1,35 +1,48 @@
+/**
+ * `code` the 2-letter ISO-639-1 code for that language
+ * `label` the name in the language
+ * `name` the English name
+ * `slug`
+ */
 export const languages = [
    {
+      code: "it",
       label: "Italiano",
       name: "Italian",
       slug: "italian",
    },
    {
+      code: "en",
       label: "English",
       name: "English",
       slug: "english",
    },
    {
+      code: "fr",
       label: "Français",
       name: "French",
       slug: "french",
    },
    {
+      code: "es",
       label: "Español",
       name: "Spanish",
       slug: "spanish",
    },
    {
+      code: "ru",
       label: "Русский",
       name: "Russian",
       slug: "russian",
    },
    {
+      code: "pt",
       label: "Português",
       name: "Portuguese",
       slug: "portuguese",
    },
    {
+      code: "sv",
       label: "Svenska",
       name: "Swedish",
       slug: "swedish",
