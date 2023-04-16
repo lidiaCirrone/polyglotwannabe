c192b6722fbac22e0e269217328378a739fb8191
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-16 18:43:25 +0200

    changed languageTiles link

--- components/languageTiles.js
+++ components/languageTiles.js
@@ -1,8 +1,6 @@
-// components
-import Link from 'next/link'
-
 // modules
 import clsx from 'clsx'
+import { useRouter } from 'next/router';
 
 // styles
 import styles from './languageTiles.module.css'
@@ -11,18 +9,25 @@ import styles from './languageTiles.module.css'
 import { languages } from '@/utils/globalVariables'
 
 export default function LanguageTiles() {
+   const router = useRouter();
+
+   const goTo = (code) => () => {
+      router.push({
+         pathname: "/hello",
+         query: { language: code }
+      })
+   }
+
+   function renderLanguageTile(language, index) {
+      return (
+         <div className={clsx([styles.tile, styles[`tile${index + 1}`]])} key={`language-${index}`} onClick={goTo(language.code)}>{language.name}</div>
+      )
+   }
+
    return (
       <section className={styles["tiles-container"]}>
          {languages.map(renderLanguageTile)}
          <div className={clsx([styles.tile, styles["tile-empty"]])}>who's next?</div>
       </section>
-   )
-}
-
-function renderLanguageTile(language, index) {
-   return (
-      <Link href={`games/${language.slug}`} key={`language-${index}`}>
-         <div className={clsx([styles.tile, styles[`tile${index + 1}`]])}>{language.name}</div>
-      </Link>
    )
 }
