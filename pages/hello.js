3f25b08fe0a9bf297b58d553617fac6850d8a517
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-16 19:59:15 +0200

    aded link to lingvo.info

--- pages/hello.js
+++ pages/hello.js
@@ -9,6 +9,7 @@ import { useRouter } from 'next/router';
 
 // utils
 import { languages } from '@/utils/globalVariables';
+import Link from 'next/link';
 
 export default function Hello() {
    const router = useRouter();
@@ -21,7 +22,8 @@ export default function Hello() {
       <Layout>
          <div className={"margin"}>
             <NavigationBar />
-            <p>You've chosen: {language.name}</p>
+            <p>You've chosen: <span className={"underline"}>{language.name}</span></p>
+            <p className={"small"}>— <Link href={`https://lingvo.info/en/lingvopedia/${language.slug}`} target={"_blank"}>find out more here!</Link> (in English)</p>
             <h2>Duolingo Stats</h2>
             <DuolingoWidget />
             {/* <Button label={"next"} href={"/hello"} /> */}
