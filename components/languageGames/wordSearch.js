f6ebf7e6cf9c0c5b0b91d6a39c66b8b6594fe041
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-21 01:05:49 +0200

    grid render

--- components/languageGames/wordSearch.js
+++ components/languageGames/wordSearch.js
@@ -15,10 +15,26 @@ import { languageGames } from '@/utils/globalVariables';
 function WordSearch({ language }) {
 
    const gameItem = languageGames[language];
+   const { matrix } = gameItem.data;
+   console.log("item: ", gameItem)
+
+   const renderGrid = () => {
+      return (
+         matrix.map((row, i) => (
+            <div key={i} className={styles.row}>
+               {row.map((column, j) => (
+                  <div key={j} className={clsx([styles.column, column.includes("-") && styles.selected])}>{column.slice(0, 1)}</div>
+               ))}
+            </div>
+         )
+         ))
+   }
 
    return (
       <>
-         <p>Portuguese game HERE</p>
+         <div className={styles.grid}>
+            {renderGrid()}
+         </div>
          {/* <Button label={"Reset"} onClick={resetState} /> */}
       </>
    )
