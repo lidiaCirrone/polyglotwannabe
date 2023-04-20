f7ce7fbfeecb9d44a4ee1224c8e46aa71431aaf7
Lidia Cirrone <lidia.cirrone@gmail.com>
2023-04-21 00:28:34 +0200

    restored dynamic route for games + missing language handling

--- pages/games/[language].js
+++ pages/games/[language].js
@@ -2,30 +2,42 @@
 import Layout from '@/components/layout'
 import NavigationBar from '@/components/ui/navigationBar'
 
+// modules
+import { useRouter } from 'next/router'
+
 // redux
 import { useSelector } from 'react-redux'
 
-// styles
-import styles from './language.module.css'
-import LanguageGame from '@/components/languageGames/wordOrder'
-import { useRouter } from 'next/router'
+// utils
+import { languageGames } from '@/utils/globalVariables';
+import Container from '@/components/ui/container'
 
 const Language = () => {
 
    const router = useRouter();
    const language = useSelector((state) => state.user.language);
-   // TO-DO: now that it's taken from redux, `router.query` may not correspond to language.slug!
-   console.log("redux language item: ", JSON.stringify(language, null, 2));
-
-   // TO-DO: check with ifReady to see it router has laready hydrated, otherwise router.query.language will be undefined!
-   console.log("router?.query?.language: ", router?.query?.language);
+   const gameLanguage = router.query.language
+   const gameItem = languageGames[gameLanguage];
 
    return (
       <Layout pageName={language.name} pageDescription={language.name}>
-         <section className={styles.container}>
+         <Container>
             <NavigationBar />
-            <LanguageGame language={router?.query?.language} />
-         </section>
+            <div>
+               {gameItem ? (
+                  <>
+                     <h2>Let's play!</h2>
+                     <p className={"margin-bottom"}>{gameItem.data.instructions}</p>
+
+                     {gameItem.component}
+                  </>
+               ) : <>
+                  <h2>{":("}</h2>
+                  <p>Unfortunately there's no game for the <span className={"bold"}>{gameLanguage}</span> language</p>
+               </>
+               }
+            </div>
+         </Container>
       </Layout>
    )
 }
