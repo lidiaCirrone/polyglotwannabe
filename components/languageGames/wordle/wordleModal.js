657800cb0f6bc7bb3ae4c1bde9a026b435e0a32f
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-03-30 18:36:50 +0100

    texts

--- components/languageGames/wordle/wordleModal.js
+++ components/languageGames/wordle/wordleModal.js
@@ -6,13 +6,13 @@ function WordleModal({ isCorrect, turn, solution }) {
       <div className={styles["modal"]}>
          {isCorrect ? (
             <div>
-               <h1>You win!</h1>
+               <h2>That's correct!</h2>
                <p className={styles["solution"]}>{solution}</p>
                <p>You found the solution in {turn} guess{turn !== 1 && 'es'} :)</p>
             </div>
          ) : (
             <div>
-               <h1>Never mind!</h1>
+               <h2>Not quite!</h2>
                <p className={styles["solution"]}>{solution}</p>
                <p>Better luck next time :)</p>
             </div>
