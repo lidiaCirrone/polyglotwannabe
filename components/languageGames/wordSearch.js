72de2ef1b9253f29133b99760f0b17de5d25aaec
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-07-04 23:59:08 +0200

    core(word-search): handles hover on adjacent tiles

--- components/languageGames/wordSearch.js
+++ components/languageGames/wordSearch.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useMemo, useState } from 'react';
+import React, { Fragment, useEffect, useMemo, useState } from 'react';
 
 // components
 import Button from '../ui/button';
@@ -24,8 +24,22 @@ function WordSearch({ language }) {
 
    const [grid, setGrid] = useState(emptyGrid);
    const [isCorrect, setIsCorrect] = useState(false);
+   const [selectedTiles, setSelectedTiles] = useState([])
+   const [hoveredTiles, setHoveredTiles] = useState([])
+
+   const isCurrentlyHovered = (r,c) => {
+    if( !hoveredTiles.length) return false
+    return hoveredTiles.find(tile => tile.r === r && tile.c === c)
+   }
 
    const toggleLetter = (r, c) => () => {
+    console.log("\n\n\ntoggleLetter")
+    console.log("selectedTiles", selectedTiles)
+    console.log("r", r)
+    console.log("c", c)
+    console.log("grid[r][c]", grid[r][c])
+      let newSelectedTiles = selectedTiles.length === 1 ? [...selectedTiles, {r,c}] : [{r,c}];
+      setSelectedTiles(newSelectedTiles);
       const updatedGrid = structuredClone(grid);
       updatedGrid[r][c] = updatedGrid[r][c] === 0 ? 1 : 0;
       setGrid(updatedGrid);
@@ -41,6 +55,30 @@ function WordSearch({ language }) {
       }
    }
 
+   function checkIfAdjacent(lastTile, currentTile) {
+    const {r: lastRow, c: lastColumn} = lastTile;
+    const {r: currentRow, c: currentColumn} = currentTile;
+    return currentRow === lastRow - 1 || /* currentRow === */ lastRow + 1
+      || currentColumn === lastColumn - 1 || lastColumn + 1
+   }
+
+   const hoverLetter = (r,c) => () => {
+    if (selectedTiles.length === 1) {
+      console.log("\n\n\nhoverLetter")
+      console.log("selectedTiles", selectedTiles)
+      if (!hoveredTiles.length || (hoveredTiles.length === 2 && checkIfAdjacent({r, c}, {r: hoveredTiles[hoveredTiles.length - 1].r, c: hoveredTiles[hoveredTiles.length - 1].c}))) {
+        setHoveredTiles(allHovered => [...allHovered, {r, c}])
+      }
+    }
+   }
+
+   useEffect(() => {
+    console.log("\n\n\nselectedTiles", selectedTiles)
+    if (selectedTiles.length === 2) {
+
+    }
+   }, [selectedTiles.length])
+
    const resetGrid = () => {
       setGrid(emptyGrid)
    }
@@ -50,7 +88,13 @@ function WordSearch({ language }) {
          matrix.map((row, r) => (
             <Fragment key={r}>
                {row.map((cell, c) => (
-                  <div key={c} className={clsx([styles.cell, "unselectable", grid[r][c] === 1 && styles.selected, isCorrect && grid[r][c] === 1 && styles.correct])} onClick={toggleLetter(r, c)}>{cell.slice(0, 1)}</div>
+                  <div key={c} className={clsx([
+                    styles.cell, 
+                    "unselectable",
+                    grid[r][c] === 1 && styles.selected, 
+                    isCorrect && grid[r][c] === 1 && styles.correct,
+                    isCurrentlyHovered(r,c) && styles.hovered
+                  ])} onClick={toggleLetter(r, c)} onMouseOver={hoverLetter(r, c)}>{cell.slice(0, 1)}</div>
                ))}
             </Fragment>
          )
